c7f2339442992afd7c2217b7e97c8d2347407fdd
Joshua Appiah Dadzie <48050933+joedemartinez@users.noreply.github.com>
2023-05-09 11:35:38 +0000

    update

--- mysql/server.js
+++ mysql/server.js
@@ -46,17 +46,11 @@ server.get("/api/users", (req, res) => {
 
 //insert data
 server.post("/api/users/add", (req, res) => {
-    let data = {
-        "user_id": 0,
-        "emp_id": req.body.emp_id,
-        "password": req.body.password,
-        "user_type": req.body.user_type,
-        "status": 0,
-        "CreatedOn": '',
-        "CreatedBy": req.body.CreatedBy
-    }
-    let sql = "INSERT INTO users_table SET ? " 
-    conn.query(sql, (err) => {
+    let {emp_id, password, user_type, status, CreatedBy} = req.body
+
+
+    let sql = "INSERT INTO users_table (emp_id, password, user_type, status, CreatedBy) VALUES (?,?,?,?,?) " 
+    conn.query(sql, [emp_id, password, user_type, status, CreatedBy], (err, results) => {
         if(err){
             res.send({status: false, message: "Oops! Error occured, user not created"})
         }else{
@@ -69,12 +63,39 @@ server.post("/api/users/add", (req, res) => {
 server.get("/api/users/:id", (req, res) => {
     let emp_id = req.params.id;
     let sql = "SELECT * FROM users_table WHERE emp_id = '" + emp_id +"'";
-    conn.query(sql, function(err, results){
+    conn.query(sql, (err, results) => {
         if(err){
             console.log(sql)
             console.log("Oops! Error occured, user not found")
         }else{
             res.send({status: true, data: results})
+        }
+    })
+})
+
+
+//update records
+server.put("/api/users/update/:id", (req, res) => {
+    let sql = "UPDATE users_table SET user_type ='" + req.body.user_type + "', status ='" + req.body.status + "' WHERE emp_id ='" +req.params.id +"'";
+
+    conn.query(sql, (err, results) => {
+        if(err) {
+            res.send ({ status: false, message: "User Update Failed"})
+        }else{
+            res.send ({ status: true, message: "User Update Successful"})
+        }
+    })
+})
+
+
+//delete records
+server.delete("/api/users/delete/:id", (req, res) => {
+    let sql = "DELETE FROM users_table WHERE emp_id ='" + req.params.id + "'";
+    conn.query(sql, (err) => {
+        if(err){
+            res.send({ status: false, message: "Users Deleted Failed"})
+        } else {
+            res.send ({ status: true, message: "User Delete Successful"})
         }
     })
 })
